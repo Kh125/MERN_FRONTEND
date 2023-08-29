1e57031fe3d780032f8938aed9c4823085c0bdb9
Khant Hmue <khanthmu152uit@gmail.com>
2023-08-29 23:33:16 +0630

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Navbar from './components/button-nav/Navbar'
+
+function App() {
+  return (
+    <div className='w-screen h-screen bg-red-300'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/button-nav/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div>
+            Button Navbar component
+        </div>
+    )
+}
+
+export default Navbar
