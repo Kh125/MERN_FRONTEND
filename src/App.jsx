46c9c51270a86395df1c12486f3c72df42616631
chanminkyaw1999 <chanwater30@gmail.com>
2023-09-01 12:11:14 +0630

    added react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import "./App.css";
-import Navbar from "./components/button-nav/Navbar";
-import Weather from "./Weather";
+
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
+
+import Weather from "./components/Weather";
+import NavBar from "./components/Navbar";
+import Schedule from "./pages/Schedule";
+import Notification from "./pages/Notification";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home"
 
 function App() {
     return (
-        <div className="w-screen h-screen bg-red-300">
-            <Navbar />
-            <Weather />
-        </div>
+        // <div className="w-screen h-screen bg-blue-400">
+        //     <Weather />
+        // </div>
+
+        <>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/schedule" element={<Schedule/>}/>
+                <Route path="/notification" element={<Notification/>}/>
+                <Route path="/profile" element={<Profile/>}/>
+            </Routes>
+            <NavBar/>
+        </Router>
+
+        </>
     );
 }
 
--- src/components/button-nav/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-const Navbar = () => {
-    return (
-        <div>
-            Button Navbar component
-        </div>
-    )
-}
-
-export default Navbar
