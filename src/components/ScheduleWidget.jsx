bb03084ac02ed418e2d201cc123fec0d4dce5d75
Sithu Kyaw <sithukyaw27500@gmail.com>
2023-09-03 14:55:05 +0630

    nothing

--- src/components/ScheduleWidget.jsx
+++ src/components/ScheduleWidget.jsx
@@ -1,30 +1,34 @@
-import React from 'react'
+import React from "react";
 
-const ScheduleWidget = (props) =>{
-    const schedule = props.schedule
+const ScheduleWidget = (props) => {
+  const schedule = props.schedule;
 
-    return <>
-        <div className="bg-custom-widget-color mx-2 mb-2 px-4 py-2 h-32 rounded-xl">
-            <div className="flex items-center h-28 justify-between">
-                <div className="flex items-center">
-                    <div className="w-1 h-24 bg-yellow-600"></div>
-                    <div className="flex-col space-y-2 mx-3 w-64">
-                        <p className="text-xl font-semibold">{schedule.Subject}</p>
-                        <p className="text-lg font-bold">{schedule.Location}</p>
-                    </div>
-                </div>
-                <div className="flex-col">
-                    <p className="text-xl font-bold">{removeAmPm(schedule.from)}</p>
-                    <p className="text-lg font-bold text-custom-color text-center">{removeAmPm(schedule.to)}</p>
-                </div>
+  return (
+    <>
+      <div className="bg-custom-widget-color mx-2 mb-2 px-4 py-2 h-32 rounded-xl">
+        <div className="flex items-center h-28 justify-between">
+          <div className="flex items-center">
+            <div className="w-1 h-24 bg-yellow-600"></div>
+            <div className="flex-col space-y-2 mx-3 w-64">
+              <p className="text-xl font-semibold">{schedule.Subject}</p>
+              <p className="text-lg font-bold">{schedule.Location}</p>
             </div>
+          </div>
+          <div className="flex-col">
+            <p className="text-xl font-bold">{removeAmPm(schedule.from)}</p>
+            <p className="text-lg font-bold text-custom-color text-center">
+              {removeAmPm(schedule.to)}
+            </p>
+          </div>
         </div>
+      </div>
     </>
-}
+  );
+};
 
 const removeAmPm = (timeString) => {
-    // Use regular expression to remove " AM" or " PM" (with space) at the end
-    return timeString.replace(/ AM$| PM$/, '');
-}
+  // Use regular expression to remove " AM" or " PM" (with space) at the end
+  return timeString.replace(/ AM$| PM$/, "");
+};
 
-export default ScheduleWidget
+export default ScheduleWidget;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,39 +1,66 @@
-import React from 'react'
+import React from "react";
 
-const Signup = ()=>{
-    return <>
-<div class="p-10">
-    <h1 class="mb-8 font-extrabold text-4xl">Register</h1>
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
-
-        <form>
+const Signup = () => {
+  return (
+    <>
+      <div class="p-10">
+        <h1 class="mb-8 font-extrabold text-4xl">Register</h1>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <form>
             <div>
-                <label class="block font-semibold" for="name">Name</label>
-                <input class="w-full shadow-inner bg-gray-100 rounded-lg placeholder-black text-2xl p-4 border-none block mt-1 w-full" id="name" type="text" name="name" required="required" autofocus="autofocus">
-                </input>
+              <label class="block font-semibold" for="name">
+                Name
+              </label>
+              <input
+                class="w-full shadow-inner bg-gray-100 rounded-lg placeholder-black text-2xl p-4 border-none block mt-1 w-full"
+                id="name"
+                type="text"
+                name="name"
+                required="required"
+                autofocus="autofocus"
+              ></input>
             </div>
 
             <div class="mt-4">
-                <label class="block font-semibold" for="email">Email</label>
-                <input class="w-full shadow-inner bg-gray-100 rounded-lg placeholder-black text-2xl p-4 border-none block mt-1 w-full" id="email" type="email" name="email" required="required">
-                </input>
+              <label class="block font-semibold" for="email">
+                Email
+              </label>
+              <input
+                class="w-full shadow-inner bg-gray-100 rounded-lg placeholder-black text-2xl p-4 border-none block mt-1 w-full"
+                id="email"
+                type="email"
+                name="email"
+                required="required"
+              ></input>
             </div>
 
             <div class="mt-4">
-                <label class="block font-semibold" for="password">Password</label>
-                <input class="w-full shadow-inner bg-gray-100 rounded-lg placeholder-black text-2xl p-4 border-none block mt-1 w-full" id="password" type="password" name="password" required="required" autocomplete="new-password">
-                </input>
+              <label class="block font-semibold" for="password">
+                Password
+              </label>
+              <input
+                class="w-full shadow-inner bg-gray-100 rounded-lg placeholder-black text-2xl p-4 border-none block mt-1 w-full"
+                id="password"
+                type="password"
+                name="password"
+                required="required"
+                autocomplete="new-password"
+              ></input>
             </div>
 
             <div class="flex items-center justify-between mt-8">
-                <button type="submit" class="flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">Register</button>
-                <a class="font-semibold">
-                    Already registered?
-                </a>
+              <button
+                type="submit"
+                class="flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+              >
+                Register
+              </button>
+              <a class="font-semibold">Already registered?</a>
             </div>
-        </form>
-    </div>
-</div>
+          </form>
+        </div>
+      </div>
     </>
-}
-export default Signup
+  );
+};
+export default Signup;
