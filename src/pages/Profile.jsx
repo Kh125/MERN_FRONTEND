fa4758521c5c23b621aca3139527efa6299cbfa5
Sithu Kyaw <sithukyaw27500@gmail.com>
2023-09-04 19:11:47 +0630

    added profile pic and update navbar icons

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,50 +5,25 @@ import { useRecoilState } from "recoil";
 import { auth } from "../shared/auth.state";
 import axios from "axios";
 import RedirectComponent from "../components/Redirect";
+import ProfileSection from "../components/ProfileSection";
 
 const Profile = () => {
   const user = useAuth();
   const [isAuth, setIsAuth] = useRecoilState(auth);
   const navigate = useNavigate();
-  
+
   const onLogout = async () => {
     await axios.post("/api/routes/logout").then(() => {
       setIsAuth(false);
       navigate("/");
     });
   };
-  
+
   return (
     <>
       {user ? (
-        <div>
-          <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
-            <li class="mr-2">
-              <a
-                href="#"
-                aria-current="page"
-                class="inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500"
-              >
-                User Profile
-              </a>
-            </li>
-            <li class="mr-2">
-              <a
-                href="#"
-                class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
-              >
-                Major
-              </a>
-            </li>
-            <li class="mr-2">
-              <a
-                href="#"
-                class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
-              >
-                Settings
-              </a>
-            </li>
-          </ul>
+        <>
+          <ProfileSection user={user} />
           <button
             onClick={onLogout}
             type="button"
@@ -56,7 +31,7 @@ const Profile = () => {
           >
             Logout
           </button>
-        </div>
+        </>
       ) : (
         <RedirectComponent />
       )}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       backgroundColor: {
-        'custom-blue': '#6484B4',
-        'custom-light-blue': '#C7DBE6',
-        'custom-dark': '#282D35',
-        'custom-widget-color': '#DEF0FF'
+        "custom-blue": "#6484B4",
+        "custom-light-blue": "#C7DBE6",
+        "custom-dark": "#282D35",
+        "custom-widget-color": "#DEF0FF",
       },
       fontSize: {
-        'custom-size-30': '30px',
-        'custom-size-60': '60px'
+        "custom-size-30": "30px",
+        "custom-size-60": "60px",
       },
       borderRadius: {
-        'custom-t': '40px'
+        "custom-t": "40px",
       },
       textColor: {
-        'custom-color': '#6484B4',
-        'custom-class-title': '#5794EE',
-        'custom-time':'#364652'
+        "custom-color": "#6484B4",
+        "custom-class-title": "#5794EE",
+        "custom-time": "#364652",
       },
     },
   },
   plugins: [],
-}
+};
